a6c39914149a31ffcc89ed022c378f2e235669c7
daoZhiXiaoku <731778645@qq.com>
2018-11-13 17:16:51 +0800

    updata

--- bookstore/users/urls.py
+++ bookstore/users/urls.py
@@ -5,4 +5,5 @@ from users import views
 urlpatterns = [ 
     url(r'^register/$', views.register, name='register'), # 用户注册
     url(r'^register_handle/$', views.register_handle, name='register_handle'), # 用户注册处理
+    url(r'^login/$', views.login, name='login'),
 ]
